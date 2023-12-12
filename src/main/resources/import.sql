6591f7a5b9fdbfc0659892e9cf42d58c7bf434ed
serp1co <manuel.salvi1911@gmail.com>
2023-12-12 11:31:25 +0100

    refactor

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,4 +3,4 @@
 -- insert into myentity (id, field) values(1, 'field-1');
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+-- alter sequence myentity_seq restart with 4; 
